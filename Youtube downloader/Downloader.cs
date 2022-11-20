357d79cdf92be429edfcc284acc0adc5ef1260b6
BryanPiek03 <45109007+BryanPiek03@users.noreply.github.com>
2022-11-20 15:19:38 +0100

    Add project files.

---
+++ Youtube downloader/Downloader.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace Youtube_downloader
+{
+    class Downloader
+    {
+        public static void start(string link, string choice)
+        {
+
+            Task.Run(async () =>
+            {
+                await download(link, choice);
+            }).GetAwaiter().GetResult();
+
+        }
+
+        private static async Task download(string link, string option)
+        {
+
+            var youtube = new YoutubeClient();
+
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
+
+            var video = await youtube.Videos.GetAsync(link);
+
+            var title = video.Title;
+
+            Console.WriteLine($"Starting downloading the {option} of {title}, You can find this in the video's folder.");
+
+            if (option == "Audio")
+            {
+                var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+                // Get the actual stream
+                var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+
+                // Download the stream to a file
+                await youtube.Videos.Streams.DownloadAsync(streamInfo, $"video's/{string.Join("_", title.Split(Path.GetInvalidFileNameChars()))} Audio.{streamInfo.Container}");
+
+                Console.WriteLine($"Done Downloading the audio of {title}, You can find this in the video's folder.");
+
+            }
+            if (option == "Video")
+            {
+                var streamInfo = streamManifest
+                .GetVideoOnlyStreams().Where(s => s.Container == Container.Mp4).GetWithHighestVideoQuality();
+
+                // Get the actual stream
+                var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+                              
+                // Download the stream to a file
+                await youtube.Videos.Streams.DownloadAsync(streamInfo, $"video's/{string.Join("_", title.Split(Path.GetInvalidFileNameChars()))} Video.{streamInfo.Container}");
+
+                Console.WriteLine($"Done Downloading the video of {title}, You can find this in the video's folder.");
+
+            }
+            if (option == "Both")
+            {
+
+                await download(link, "Audio");
+                await download(link, "Video");
+
+            }
+        }
+
+    }
+}
---
+++ Youtube downloader/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Youtube_downloader
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+
+            Console.Title = "YT Downloader";
+
+            Console.Clear();
+
+
+            Console.Write("\n\n\n(A) Single Video Mode\n(B) Multi Video Mode\n\n> ");
+            string mode = Console.ReadLine().ToUpper();
+
+            Console.Write("\n\nCurrently we only support Video and/or Audio only.\nSelecting both will download audio and video seperately.\n");
+            Console.Write("(A) Audio Only\n");
+            Console.Write("(B) Video Only\n");
+            Console.Write("(C) Both\n\n");
+            Console.Write("> ");
+
+            string choice = Console.ReadLine().ToUpper();
+
+            if (choice == "A")
+                choice = "Audio";
+            else if (choice == "B")
+                choice = "Video";
+            else if (choice == "C")
+                choice = "Both";
+            else
+                Environment.Exit(0);
+
+            if (mode == "A")
+            {
+
+                Console.Write("\n\n\nInsert Video URL: ");
+                string link = Console.ReadLine();
+
+                Downloader.start(link, choice);
+            }
+            if(mode == "B")
+            {
+
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog.Title = "Select Link file:";
+
+                    if(openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string filePath = openFileDialog.FileName;
+
+                        foreach(string line in File.ReadAllLines(filePath))
+                        {
+
+                            Downloader.start(line, choice);
+                            Thread.Sleep(2000);
+
+                        }
+                    }
+                }
+
+            }
+
+            Console.WriteLine("\n\nAll Done");
+
+            Console.ReadLine();
+            Console.Clear();
+            Main();
+
+        }
+
+        
+    }
+}
